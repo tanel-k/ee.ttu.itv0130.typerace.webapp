3fc63721082052346632af9bc89f58dc2a3596e7
Tanel Prikk <tanel.prikk@outlook.com>
2017-05-16 18:33:49 +0300

    saving progress

--- src/components/history-sidebar/history-sidebar.js
+++ src/components/history-sidebar/history-sidebar.js
@@ -1,3 +1,57 @@
-export class HistorySidebar {
+import { inject } from 'aurelia-framework';
 
+import 'jquery';
+
+@inject(Element)
+export class HistorySidebar {
+  constructor(element) {
+    this.element = element;
+  }
+
+  attached() {
+    this.initDOMHooks();
+    this.attachEventListeners();
+  }
+
+  initStateModel() {
+    this.isActive = false;
+  }
+
+  initDOMHooks() {
+    this.historyContainer = this.element.querySelector('.history-container');
+    this.closeButton = this.element.querySelector('.close-button');
+  }
+
+  attachEventListeners() {
+    this.historyContainer.addEventListener('click', () => {
+      this.activateHistoryContainer();
+    });
+/*
+    this.closeButton.addEventListener('click', () => {
+      this.deactivateHistoryContainer();
+    });
+*/
+  }
+
+  activateHistoryContainer() {
+    if (!this.isActive) {
+      this.historyContainer.classList.add('active');
+      const overlayDiv = document.createElement('div');
+      overlayDiv.classList.add('page-overlay');
+      document.body.appendChild(overlayDiv);
+      overlayDiv.addEventListener('click', () => this.deactivateHistoryContainer());
+      this.isActive = true;
+    }
+  }
+
+  deactivateHistoryContainer() {
+    if (this.isActive) {
+      this.historyContainer.classList.remove('active');
+      const overlayDiv = document.querySelector('.page-overlay');
+      if (overlayDiv) {
+        document.body.removeChild(overlayDiv);
+      }
+      this.isActive = false;
+    }
+  }
 }
